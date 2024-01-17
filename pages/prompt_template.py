245d4271f9aa385c15b7f477854dd142dcde2228
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-17 22:43:41 +0100

    Update prompt_template.py

--- pages/prompt_template.py
+++ pages/prompt_template.py
@@ -7,7 +7,19 @@ api_key1 = st.secrets["OPENAI_API_KEY"]
 
 chat_model = ChatOpenAI(openai_api_key=api_key1)
 
-template = "You area helpful assitant that translates {input_Language} to {output_Language}."
+template = """"
+    You area helpful assistant for onboarding new talent in a company of generative artificial intelligence.
+    The name of the company you work for is GenAI Partners.
+    Your name as virtual assistant is Wilber and you are the host for the new ones.
+    They will tell you their {user_name} and their {user_rol} and you will welcoming them and guide them.
+    You have to guide the new talent to give 1) orientation, 2) precedents and  3) training for their new rol in the company.
+    When you give orientation you explain the kind of mission and vision for a generative artificial intelligence company.
+    When you give precedents you explain that the rol does
+    When you give training you select the top 5 books about the rol
+    Then you tell the new talent that the day for star working is thursday january 18 of 2024.
+    Finally ask by the name of the new talent if he o she has questions about the process and if they have all the tests done to start the job. 
+    """
+
 human_template = "{text}"
 
 chat_prompt = ChatPromptTemplate.from_messages([
@@ -15,9 +27,9 @@ chat_prompt = ChatPromptTemplate.from_messages([
   ("human", human_template),
 ])
 
-messages = chat_prompt.format_messages(input_Language="English",
-                                       output_Language="French",
-                                       text="I love programming recommender systems")
+messages = chat_prompt.format_messages(user_name="Wilber Jimenez Hernandez",
+                                       user_rol="Machine Learning Engineer",
+                                       text="Please give me tips for my onboarding to the company")
 
 result = chat_model.predict_messages(messages)
 
