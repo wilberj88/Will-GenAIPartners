c6075d5a33a9fa8df9cd47b631b57a3e18b85f7b
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-17 23:13:30 +0100

    Update home.py

--- home.py
+++ home.py
@@ -2,6 +2,20 @@
 import streamlit as st
 from langchain.chat_models import ChatOpenAI
 import os
+from st_pages import Page, show_pages, add_page_title
+
+# Optional -- adds the title and icon to the current page
+add_page_title()
+
+# Specify what pages should be shown in the sidebar, and what their titles and icons
+# should be
+show_pages(
+    [
+        Page("streamlit_app.py", "Home", "🏠"),
+        Page("other_pages/page2.py", "Page 2", ":books:"),
+    ]
+)
+
 
 #API CONNECTION
 api_key1 = st.secrets["OPENAI_API_KEY"]
