46ae14680d8afa5c4d15b94646a9436b66d09d24
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-17 21:35:43 +0100

    Update simple.py

--- pages/simple.py
+++ pages/simple.py
@@ -13,4 +13,7 @@ result = chat_model.predict("Hi my name is Wilber and I am new employee as in Ge
 
 st.title('Guide by Virtual Assistant')
 st.header('Personalized by my name, rol and kind of company (GenAI Partners)')
+with st.expander("See the prompt:")
+  st.write("Hi my name is Wilber and I am new employee as in GenAI Partnerts Consultants, please guide me in the process to understand how a generative artificial intelligence company works and what a machine learning engineer does, also give me tips for orientation, precedents and training for my new rol")
+st.divider()
 st.write(result)
