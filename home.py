37bfb4baf6fe63a431d6fa0365af2b1b9d02a775
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-17 22:03:20 +0100

    Update home.py

--- home.py
+++ home.py
@@ -21,15 +21,17 @@ st.divider()
 st.title('Personalized Guide by Generative Virtual Assistant')
 st.header('Please select your rol an receive a personalized onboarding guide')
 
-#INPUT
-input = st.selectbox('Select your rol', rol, key = 'rol_de_entrada')
+#INPUTS
+input_name = st.text_input("Please write your name:")
+input_rol = st.selectbox('Plase select your rol', rol, key = 'rol_de_entrada')
+
 
 #PROMPT
 with st.expander("See the prompt"):
-  st.write("Hi my name is Wilber and I am new employee as in GenAI Partnerts Consultants, please guide me in the process to understand how a generative artificial intelligence company works and what a (rol_selected) does, also give me tips for orientation, precedents and training for my new rol")
+  st.write("Hi my name is (name_typed) and I am new employee as in GenAI Partnerts Consultants, please guide me in the process to understand how a generative artificial intelligence company works and what a (rol_selected) does, also give me tips for orientation, precedents and training for my new rol")
 st.divider()
 st.write('Virtual Assistant 🤖 says:')
 
 #SHOW BOTs MESSAGES
-result = chat_model.predict("Hi my name is Wilber and I am new employee as in GenAI Partnerts Consultants, please guide me in the process to understand how a generative artificial intelligence company works and what a {input} does, also give me tips for orientation, precedents and training for my new rol")
+result = chat_model.predict("Hi my name is {input_name} and I am new employee as in GenAI Partnerts Consultants, please guide me in the process to understand how a generative artificial intelligence company works and what a {input} does, also give me tips for orientation, precedents and training for my new rol")
 st.write(result)
