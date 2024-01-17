c0d32f7b60f579363ccaf798349456fa57f0af91
Wilber Jiménez Hernández <49035216+wilberj88@users.noreply.github.com>
2024-01-17 22:57:10 +0100

    Update home.py

--- home.py
+++ home.py
@@ -22,26 +22,4 @@ st.set_page_config(
 st.title("Exercise for GenAI Partners")
 st.header("By Wilber Jiménez Hernández at january 17/2024")
 st.divider()
-st.title('Personalized Guide by Generative Virtual Assistant')
-
-#INPUTS
-#input_name = st.text_input("Please write your name:")
-input_rol = st.selectbox(
-  'Plase select your rol for a personalized onboarding guide', 
-  rol, 
-  index=None,
-  placeholder="Choose an option")
-
-
-#PROMPT
-prompt = "Hi I am new employee as in GenAI Partnerts Consultants, please guide me in the process to understand how a generative artificial intelligence company works and what a {input_rol} does, also give me tips for orientation, precedents and training for my new rol"
-with st.expander("See the prompt for this use case"):
-  st.write(prompt)
-st.divider()
-
-
-#SHOW BOTs MESSAGES
-st.write('Virtual Assistant 🤖 says:')
-result = chat_model.predict(prompt)
-if input_rol:
-  st.write(result)
+st.title('3 differents implementation of Personalized Guide by Generative Virtual Assistant')
